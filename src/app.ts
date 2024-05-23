b97c219d6242aff9a48afe642421992539adc5ce
Minh Vương <hardingadonis@gmail.com>
2024-05-23 13:41:13 +0700

    add: add package for hot reloading
    add: `ts-node` and `nodemon` packages

--- src/app.ts
+++ src/app.ts
@@ -1,49 +1,13 @@
-const express = require('express');
+import express, { Express, Request, Response } from 'express';
 
-const app = express();
+const app: Express = express();
 const port = 3000;
 
-app.get('/', (req: any, res: any) => {
-	res.send('Hello World!');
-
-	console.log('GET: /');
+app.get('/', (req: Request, res: Response) => {
+	console.log(req);
+	res.send('Express + TypeScript Server');
 });
-
-app.get('/items', (req: any, res: any) => {
-	res.send('Items list');
-
-	console.log('GET: /items');
-});
-
-app.post('/items', (req: any, res: any) => {
-	res.status(201).send('Item created');
-
-	console.log('POST: /items');
-});
-
-app.get('/items/:id', (req: any, res: any) => {
-	res.send('Item details');
-
-	console.log('GET: /items/:id');
-});
-
-app.put('/items/:id', (req: any, res: any) => {
-	res.send('Item updated');
-
-	console.log('PUT: /items/:id');
-});
-
-app.delete('/items/:id', (req: any, res: any) => {
-	res.send('Item deleted');
-
-	console.log('DELETE: /items/:id');
-});
-
-const swaggerUI = require('swagger-ui-express');
-const swaggerDocument = require('./swagger-output.json');
-
-app.use('/swagger', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.listen(port, () => {
-	console.log(`Server is running at http://localhost:${port}`);
+	console.log(`[server]: Server is running at http://localhost:${port}`);
 });
